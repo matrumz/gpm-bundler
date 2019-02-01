358ba09a973499efc83006da009b2b8bde0c5aae
matrumz <matrumz@hotmail.com>
2019-01-31 22:18:39 -0500

    Size of bundle now exposed from class

--- src/libs/Bundler.py
+++ src/libs/Bundler.py
@@ -3,11 +3,12 @@ import math
 
 class Bundler:
     prefix = "_Bundle-"
+    bundle_size = 1000
 
     def generate_bundles(tracks):
         current_dt = datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
         bundle_name = Bundler.prefix + (input("Enter bundle name (%s): " % current_dt) or current_dt)
 
-        sets = range(math.ceil(len(tracks)/1000))
+        sets = range(math.ceil(len(tracks)/Bundler.bundle_size))
         for set in sets:
             print(bundle_name + ("" if len(sets) == 1 else "-"+str(set+1)))
