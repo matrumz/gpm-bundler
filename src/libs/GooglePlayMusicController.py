4fef6aac1311603340c2852e77599fedb21bafc0
matrumz <matrumz@hotmail.com>
2019-01-31 21:02:44 -0500

    Getting list of unique trackIds already bundled.

--- src/libs/GooglePlayMusicController.py
+++ src/libs/GooglePlayMusicController.py
@@ -1,8 +1,12 @@
 from gmusicapi import Mobileclient
 import getpass
+import re
+from functools import reduce
 
 class Session:
     # Private vars
+    # _bundle_prefix = "_Bundle-"
+    _bundle_prefix = "Super"
 
     # Public vars
     api = None
@@ -14,13 +18,24 @@ class Session:
             user = input("Please enter Google account: ")
         if not pw:
             pw = getpass.getpass("Please enter password for %s: " % user)
+        print("Logging into Google Play Music as %s..." % user, end='', flush=True)
         self.logged_in = self.api.login(user, pw, Mobileclient.FROM_MAC_ADDRESS)
         if self.logged_in:
-            print("Google Play Music login successful.")
+            print(" successful!")
         else:
-            print("Google Play Music login FAILED.")
+            print(" FAILED.")
 
     def __del__(self):
         if self.logged_in:
-            print("Logging out of Google Play Music...")
+            print("Logging out of Google Play Music...", end='', flush=True)
             self.api.logout()
+            print(" Done!")
+
+    def get_bundled_tracks(self):
+        playlists = self.api.get_all_user_playlist_contents()
+        bundles = list(filter(lambda pl: re.match(self._bundle_prefix + ".*", pl["name"]), playlists))
+        track_ids = []
+        if len(bundles):
+            tracks = reduce(lambda _tracks, bundle_x: _tracks + bundle_x["tracks"], bundles, [])
+            track_ids = list(set(list(map(lambda track: track["trackId"], tracks))))
+        return track_ids
