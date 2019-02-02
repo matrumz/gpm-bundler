faada9681cb20fcc4eb0f86d91b562c193e703f6
matrumz <matrumz@hotmail.com>
2019-02-02 13:57:40 -0500

    Generating bundles

--- src/libs/Bundler.py
+++ src/libs/Bundler.py
@@ -7,8 +7,10 @@ class Bundler:
 
     def generate_bundles(tracks):
         current_dt = datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
-        bundle_name = Bundler.prefix + (input("Enter bundle name (%s): " % current_dt) or current_dt)
+        bundle_base_name = Bundler.prefix + (input("Enter bundle name (%s): " % current_dt) or current_dt)
 
         sets = range(math.ceil(len(tracks)/Bundler.bundle_size))
         for set in sets:
-            print(bundle_name + ("" if len(sets) == 1 else "-"+str(set+1)))
+            bundle_name = bundle_base_name + ("" if len(sets) == 1 else "-"+str(set+1))
+            bundle_contents = tracks[set*Bundler.bundle_size : ((set+1)*Bundler.bundle_size)]
+            yield bundle_name, bundle_contents
